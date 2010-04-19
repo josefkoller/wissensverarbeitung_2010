4b2546c8eccb4cdefc68c02ec57f32c46a55a796
josef_koller <josef.koller@student.tugraz.at>
2010-04-19 10:18:12 +0200

    added tiny comment to plotErr

--- 02/plotErrorFunction.m
+++ 02/plotErrorFunction.m
@@ -9,8 +9,8 @@ error = getError(w1, w2);
 
 figure;
 contour(x, y, error, 30); % x,y ... coordinates of the surface; 
-                                    % error... heights above the plane
-
+                                    % error... heights above the plane;
+                                    % error = f(x,y)
 hold all;
 handle(1) = plot(weights(:,1), weights(:,2), 'ro-', 'LineWidth', 3); % plotting first and second columns of all rows
 handle(2) = plot(weights(end,1), weights(end,2), 'ks', 'MarkerSize', 10, 'LineWidth', 3); % plotting last row ... end vector
