c65df501e88a32bd279f99698ca484dec326181c
josef_koller <josef.koller@student.tugraz.at>
2010-04-19 14:43:56 +0200

    minor: rm unneeded line

--- 02/hw_2_1_3.m
+++ 02/hw_2_1_3.m
@@ -5,7 +5,6 @@ w0 = [-0.2, -0.5];
 eta_list = 0.4:-0.1:0.1; % step size
 numIter = 100;
 
-weights_matrix = zeros(length(eta_list), numIter+1);
 for eta_index = 1:length(eta_list)
     eta = eta_list(eta_index);
     [weights, error] = gradientDescentHw2(w0, numIter, eta);
