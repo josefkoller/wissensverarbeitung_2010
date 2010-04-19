09e55872fc4c793b0567de44772e30b97c266dd2
josef_koller <josef.koller@student.tugraz.at>
2010-04-19 10:15:10 +0200

    added some comments to plotErrorFunction

--- 02/plotErrorFunction.m
+++ 02/plotErrorFunction.m
@@ -8,17 +8,15 @@ w2 = w1';
 error = getError(w1, w2);
 
 figure;
-contour(x, y, error, 30);
+contour(x, y, error, 30); % x,y ... coordinates of the surface; 
+                                    % error... heights above the plane
 
 hold all;
-handle(1) = plot(weights(:,1), weights(:,2), 'ro-', 'LineWidth', 3);
-
-handle(2) = plot(weights(end,1), weights(end,2), 'ks', 'MarkerSize', 10, 'LineWidth', 3);
-handle(3) = plot(weights(1,1), weights(1,2), 'ko', 'MarkerSize', 10, 'LineWidth', 3);
-
+handle(1) = plot(weights(:,1), weights(:,2), 'ro-', 'LineWidth', 3); % plotting first and second columns of all rows
+handle(2) = plot(weights(end,1), weights(end,2), 'ks', 'MarkerSize', 10, 'LineWidth', 3); % plotting last row ... end vector
+handle(3) = plot(weights(1,1), weights(1,2), 'ko', 'MarkerSize', 10, 'LineWidth', 3); % plotting first row .. start vector
 legend(handle, 'Weights', 'EndVector', 'StartVector', 'Location', 'SouthEast');
 
 function [error] = getError(w1, w2)
-
-
-error = - 2 * exp(-5 * (0.5*(w1 - 1).^2 +  (w2 - 1).^2)) - 1 *  exp(-0.1 * (2 * (w1 + 1).^2 *2 +  (w2 ).^2/2));
+error = - 2 * exp(-5 * (0.5*(w1 - 1).^2 + (w2 - 1).^2)) - ...
+    1 *  exp(-0.1 * (2 * (w1 + 1).^2 *2 +  (w2 ).^2/2));
