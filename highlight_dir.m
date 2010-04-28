454d6026c89cd12dada8fe532a7e21d6054e2dcd
josef_koller <josef.koller@student.tugraz.at>
2010-04-28 21:33:12 +0200

    Bugfix highlight_dir

--- highlight_dir.m
+++ highlight_dir.m
@@ -1,10 +1,9 @@
 function highlight_dir( dir )
     s = what(dir);
     m_files = s.m;
-    cd(dir);
     for i = 1 : length(m_files)
-        m_file = m_files{i};
-        tex_file = strrep(m_file, '.m', '.tex');
+        m_file = [dir '/' m_files{i}]
+        tex_file = strrep(m_file, '.m', '.tex')
         highlight(m_file, 'tex', tex_file);
     end
 end
