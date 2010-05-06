147d64bef868c9177a6a634bc0945757cc03128e
josef_koller <josef.koller@student.tugraz.at>
2010-05-06 23:37:24 +0200

    fixed mse title

--- 03/plot_mse.m
+++ 03/plot_mse.m
@@ -1,16 +1,15 @@
 function [] = plot_mse(neuron_number, mse_train_list, mse_test_list, plot_filename_prefix)
     clf reset; 
     
-    hold on; 
      
     plot(mse_train_list, 'r');
-    title('MSE over the used neuron count - training data');
-    ylabel('Mean Squared Error with Regularization (msereg)');
+    title('MSEs over the used neuron count');
+    ylabel('Mean Squared Error');
     xlabel('# neuron');
   
+    hold on; 
     plot(mse_test_list, 'b');
-    title('MSE over the used neuron count - test data');
-    ylabel('Mean Squared Error with Regularization (msereg)');
+    ylabel('Mean Squared Error');
     xlabel('# neuron');
   
     legend('MSE training data', 'MSE test data');
