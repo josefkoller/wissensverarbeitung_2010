0742bb2fa6cd0b8ca9706922bdb2f14c660b9a43
Christof Sirk <sirchb01@gmail.com>
2010-05-06 15:59:43 +0200

    Don't print the filesnames which have been formatted.

--- highlight_dir.m
+++ highlight_dir.m
@@ -2,8 +2,8 @@ function highlight_dir( dir )
     s = what(dir);
     m_files = s.m;
     for i = 1 : length(m_files)
-        m_file = [dir '/' m_files{i}]
-        tex_file = strrep(m_file, '.m', '.tex')
+        m_file = [dir '/' m_files{i}];
+        tex_file = strrep(m_file, '.m', '.tex');
         highlight(m_file, 'tex', tex_file);
     end
 end
