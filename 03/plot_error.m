bba52bec4ec5b991e99922c0c3d78ba2e3eb2f11
josef_koller <josef.koller@student.tugraz.at>
2010-05-07 08:16:50 +0200

    3.1 plotting mse over the number of neurons

--- 03/plot_error.m
+++ 03/plot_error.m
@@ -1,14 +1,25 @@
 function [] = plot_error(error_test, error_training, ...
-    figure_title, x_label, plot_filename_prefix)
+    figure_title, x_label, plot_filename_prefix, x)
     figure;
      
-    plot(error_training, 'r');
+    if exist('x', 'var')
+        plot(x, error_training, 'r');
+    else
+        plot(error_training, 'r');
+    end
+
     title(figure_title);
     ylabel('Error');
     xlabel(x_label);
   
     hold on; 
-    plot(error_test, 'b');
+    
+     if exist('x', 'var')
+        plot(x, error_test, 'b');
+    else
+        plot(error_test, 'b');
+    end
+    
     ylabel('Error');
     xlabel(x_label);
   
--- 03/problem_3_1.m
+++ 03/problem_3_1.m
@@ -14,19 +14,20 @@ if exist('SPEED', 'file')
     epoch_count = 50;
 end
 
+mse_train_list = [];
+mse_test_list = [];
+
 for neuron_count = neuron_count_list
     [ network, performance ] = create_and_train_network( x_min, x_max, ...
         neuron_count, performance_function, epoch_count, ...
         x_train, y_train, x_test, y_test);
 
     mse_train = sum((y_train - sim(network, x_train)).^2) / length(x_train);
+    mse_train_list(length(mse_train_list)+1) = mse_train;
     mse_test = sum((y_test - sim(network, x_test)).^2) / length(x_test);
-
+    mse_test_list(length(mse_test_list)+1) = mse_test;
+    
     plot_filename_prefix = sprintf('3_1_%d_neurons', neuron_count);
-    figure_title = sprintf('MSE for the number of epochs used - Simple Regression - neurons: %d', neuron_count);
-    error_x_label = '# epochs';
-    plot_error(performance.tperf, performance.perf, ...
-        figure_title, error_x_label, plot_filename_prefix); 
     
     % check trained network with new input
     x = x_min:x_step:x_max;
@@ -37,4 +38,14 @@ for neuron_count = neuron_count_list
         figure_title, plot_filename_prefix)
 end
     
+
+plot_file_name_prefix = 'mse_for_neuron_counts';
+x = neuron_count_list;
+sprintf('size(x): %d', length(x) )
+sprintf('size(mse_train_list): %d', length(mse_train_list))
+
+figure_title = sprintf('MSE for the number of epochs used - Simple Regression - neurons: %d', neuron_count);
+error_x_label = '# epochs';
+plot_error(mse_train_list, mse_test_list, ...
+   figure_title, error_x_label, plot_filename_prefix, x); 
 
